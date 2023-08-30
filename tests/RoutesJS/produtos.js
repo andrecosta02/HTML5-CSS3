61a37a153bcb4a225838c3c317c54f851a14aafe
andrecosta02 <131416835+andrecosta02@users.noreply.github.com>
2023-08-30 12:15:41 -0300

    Começando estudos com Bootstrap

---
+++ tests/RoutesJS/produtos.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+const path = require('path')
+
+
+router.get('/', (req, res, next) => {
+	res.sendFile(path.join(__dirname, '../', 'views', 'produtos', 'produtos.html'))
+})
+
+router.get('/add_produto', (req, res, next) => {
+	res.sendFile(path.join(__dirname, '../', 'views', 'produtos', 'add_produto.html'))
+})
+
+module.exports = router
